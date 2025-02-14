b4bf20500241b1dd89ef16f3191c75cb791c1125
Federjj <jonebillsh@gmail.com>
2025-02-13 23:36:35 -0500

    ignorar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
 import { MenuComponent } from './pages/menu/menu.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
     {path: '', component: LandingComponent},
-    {path: 'menu', component: MenuComponent}
+    {path: 'menu', component: MenuComponent},
+    {path: 'dashboard', component: DashboardComponent}
 ];
